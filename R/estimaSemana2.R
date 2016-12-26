6ec1c03577dd9a0cd344fa251b82dc8069f66ffb
Pablo Martin Marin Abanto <boosterpablo@gmail.com>
2016-12-26 14:02:01 -0500

    Update estimaSemana2.R
    Correción

--- R/estimaSemana2.R
+++ R/estimaSemana2.R
@@ -6,12 +6,10 @@ estimaSemana2 <- function(inicio.temp, fin.temp, ...){
   diasTemporada = seq.Date(as.Date(inicio.temp), as.Date(fin.temp),by =  "day")
   dias =  weekdays(diasTemporada)
   temporada = data.frame(diasTemporada = diasTemporada, dias = dias)
-  inicio.semana = which(temporada$dias[1:7] %in% "lunes")
-  num.semana = round((length(temporada$dias)-inicio.semana-inicio.semana)/ 7)
-  residuo = length(temporada$dias) - (inicio.semana + num.semana*7)
-  temporada$semana = c(rep(0, inicio.semana-1),rep(1:num.semana, each = 7), rep(num.semana + 1, residuo+1))
-  
-  
+  nSem = c(rep(0, min(which(dias == "lunes"))-1), sort(rep(1:length(dias[dias=="lunes"]),7)))
+  nSem[1:length(dias)]
+  temporada$semana <- nSem[1:length(dias)]  
   return(temporada)
+}
 }
 
