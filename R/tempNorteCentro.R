3659c42fcc0d6c00f8e0f5a8714f0a4f1cbb659e
Pablo Martin Marin Abanto <boosterpablo@gmail.com>
2016-12-27 13:48:10 -0500

    fenix

--- R/tempNorteCentro.R
+++ R/tempNorteCentro.R
@@ -1,20 +1,20 @@
 ## tempNorteCentro
 
-DateTemp <- function(x){
-  require(fenix)
-  temporada$temp_1_i <- as.Date(temporada$temp_1_i, format ="%d/%m/%Y") 
-  temporada$temp_1_f <- as.Date(temporada$temp_1_f, format ="%d/%m/%Y") 
-  temporada$temp_2_i <- as.Date(temporada$temp_2_i, format ="%d/%m/%Y") 
-  temporada$temp_2_f <- as.Date(temporada$temp_2_f, format ="%d/%m/%Y") 
+#DateTemp <- function(x){
+#  require(fenix)
+#  temporada$temp_1_i <- as.Date(temporada$temp_1_i, format ="%d/%m/%Y") 
+#  temporada$temp_1_f <- as.Date(temporada$temp_1_f, format ="%d/%m/%Y") 
+#  temporada$temp_2_i <- as.Date(temporada$temp_2_i, format ="%d/%m/%Y") 
+#  temporada$temp_2_f <- as.Date(temporada$temp_2_f, format ="%d/%m/%Y") 
   
-  id.temp = rep(NA, length(x))
-  for(i in 1:length(temporada$Años)){
-    if(temporada$Años[i] == 2014){
-      id.temp[x >= temporada$temp_1_i[i] & x <= temporada$temp_1_f[i]] =   paste(temporada$Años[i],".1", sep = "")
-    }else{
-      id.temp[x >= temporada$temp_1_i[i] & x <= temporada$temp_1_f[i]] =   paste(temporada$Años[i],".1", sep = "")
-      id.temp[x >= temporada$temp_2_i[i] & x <= temporada$temp_2_f[i]] =   paste(temporada$Años[i],".2", sep = "")
-    }
-  }
- return(id.temp) 
-}
+#  id.temp = rep(NA, length(x))
+#  for(i in 1:length(temporada$Años)){
+#    if(temporada$Años[i] == 2014){
+#      id.temp[x >= temporada$temp_1_i[i] & x <= temporada$temp_1_f[i]] =   paste(temporada$Años[i],".1", sep = "")
+#    }else{
+#      id.temp[x >= temporada$temp_1_i[i] & x <= temporada$temp_1_f[i]] =   paste(temporada$Años[i],".1", sep = "")
+#      id.temp[x >= temporada$temp_2_i[i] & x <= temporada$temp_2_f[i]] =   paste(temporada$Años[i],".2", sep = "")
+#    }
+#  }
+# return(id.temp) 
+#}
