19807ad36a7041d2395242fc88b75783d6655dab
Pablo Martin Marin Abanto <boosterpablo@gmail.com>
2018-11-06 16:43:38 -0500

    ...
    ...

--- R/All_distancias.R
+++ R/All_distancias.R
@@ -20,50 +20,7 @@ latlon.to.m =
     end
   }
 
-distanciaSigno <- function (x, y, x1, y1) 
-{
-  
-  junk = approx(y,x,y1)
-  signo <- (junk$y-x1)/abs((junk$y-x1))
-  
-  A3 = y1 * pi/180
-  B3 = x1 * pi/180
-  A31 = y * pi/180
-  B31 = x * pi/180
-  num2 = length(x1) - 1
-  n = length(x)
-  dfinal = NULL
-  num = length(y)
-  pos.na = c(which(is.na(y) == TRUE), which(is.na(x) == TRUE))
-  pos.na
-  dc <- NULL
-  num2 = length(x1) - 1
-  n = length(x)
-  dfinal = NULL
-  num = length(y)
-  pos.na = c(which(is.na(y) == TRUE), which(is.na(x) == TRUE))
-  pos.na
-  dc <- NULL
-  for (i in 1:num) {
-    A = NULL
-    B = NULL
-    A = sin(A3[1:num2]) * sin(A31[i])
-    B = cos(A3[1:num2]) * cos(A31[i]) * cos(B3[1:num2] - 
-                                              B31[i])
-    grad = acos(A + B)
-    gradinrad = 180 * grad/pi
-    distancia = (60 * gradinrad)
-    indnonan = which(distancia > 0)
-    dist_min = min(distancia[indnonan])
-    dfinal[i] = (dist_min)
-    dc <- rbind(dc, dist_min)
-  }
-  dc[is.infinite(dc)] <- NA
-  #return(dc)
-  dc <- as.numeric(dc)
-  dc.signo <- dc*signo
-  list(dc = dc, dc.signo = dc.signo)
-}
+
  
 # junk = approx(shoreline$V2,shoreline$V1,bitacora1$lat)
 # bitacora1$distttt = latlon.to.m(junk$y,bitacora1$lat,bitacora1$lon,bitacora1$lat)/1000
--- R/growth.R
+++ R/growth.R
@@ -11,23 +11,3 @@
 #   return(moda)
 # }
 
-estima.modas <- function(tallas, Lmin, Lmax, dL, umbral = 10){
-  moda=NULL
-  for(i in 1:length(tallas[,1])){
-    vector <- as.numeric(tallas[i,])
-    moda0  <- which(diff(sign(diff(vector)))==-2)+1
-    if(length(vector[moda0]) > 1){
-      for(i in 1:length(vector[moda0])){
-        if(vector[moda0[i]] < umbral){
-          moda0[i] = NA
-        }
-      }
-    }
-    moda0  <- moda0[!is.na(moda0)]
-    Lmoda0 <- seq(Lmin,Lmax,by=dL)[moda0]
-    Lmoda0 <- Lmoda0[order(vector[moda0], decreasing=TRUE)]
-    Lmoda0 <- c(Lmoda0,rep(NA,10-length(moda0)))
-    moda   <- rbind(moda,Lmoda0) 
-  }  
-  return(moda)
-} 
