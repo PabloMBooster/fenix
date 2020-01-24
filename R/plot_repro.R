f396305d68d361b7a8be6a1826d1184797b420a4
Pablo Martin Marin Abanto <boosterpablo@gmail.com>
2020-01-24 14:13:41 -0500

    update

--- R/plot_repro.R
+++ R/plot_repro.R
@@ -1,5 +1,5 @@
 plot_repro <- function(tiempo, indice, patron, sd_patron, valor_crucero = NULL, fecha = as.Date(time, format = "%d/%m/%Y"), 
-                       nombre_indice = NULL, nombre_indice_legenda = NULL,  nombre_patron = NULL, plot_patron = TRUE,
+                       nombre_indice = NULL, legend = TRUE, nombre_indice_legenda = NULL,  nombre_patron = NULL, plot_patron = TRUE,
                         nombre_crucero = NULL, axis_x = TRUE, dl = 3){
   require(plotrix)
   x <- 1:length(indice)
@@ -48,26 +48,35 @@ plot_repro <- function(tiempo, indice, patron, sd_patron, valor_crucero = NULL, 
   axis(2)
   
   box()
+  
+  
   if(is.null(nombre_indice_legenda)){
     nombre_indice_legenda <- nombre_indice
   }
+  
   if(is.null(nombre_patron)){
     nombre_patron <- "Patron"
   }
-  if(plot_patron == TRUE){
-    
-  if(is.null(nombre_crucero)){
-    legend("toprigh",c(nombre_patron, nombre_indice_legenda), lty = c(1,1), lwd = c(2,2),col = c(2,4),bty = "n")
-  }else{
-    legend("toprigh",c(nombre_patron, nombre_indice_legenda, nombre_crucero), lty = c(1,1,0), lwd = c(2,2,NA),pch = c(NA,NA,16), col = c(2,4,1),bty = "n")
-  }
-  }
-  if(plot_patron == FALSE){
-    
-    if(is.null(nombre_crucero)){
-      legend("toprigh", nombre_indice_legenda, lty = 1, lwd = 2, col = 4, bty = "n")
-    }else{
-      legend("toprigh",c(nombre_indice_legenda, nombre_crucero), lty = c(1,0), lwd = c(2,NA), pch = c(NA,16), col = c(4,1), bty = "n")
+  
+  if(legend == TRUE){
+    if(plot_patron == TRUE){
+      
+      if(is.null(nombre_crucero)){
+        legend("toprigh",c(nombre_patron, nombre_indice_legenda), lty = c(1,1), lwd = c(2,2),col = c(2,4),bty = "n")
+      }else{
+        legend("toprigh",c(nombre_patron, nombre_indice_legenda, nombre_crucero), lty = c(1,1,0), lwd = c(2,2,NA),pch = c(NA,NA,16), col = c(2,4,1),bty = "n")
+      }
     }
+    if(plot_patron == FALSE){
+      
+      if(is.null(nombre_crucero)){
+        legend("toprigh", nombre_indice_legenda, lty = 1, lwd = 2, col = 4, bty = "n")
+      }else{
+        legend("toprigh",c(nombre_indice_legenda, nombre_crucero), lty = c(1,0), lwd = c(2,NA), pch = c(NA,16), col = c(4,1), bty = "n")
+      }
+    }
+    
   }
+  
+  
 }
